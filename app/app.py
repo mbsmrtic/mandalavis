14536b7d8448f5ca6beb9d0f9eee6cd989592b83
Mary Beth Smrtic <mbsmrtic@gmail.com>
2025-04-06 11:56:10 -0400

    added favicon route

--- app/app.py
+++ app/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask import render_template
-from flask import url_for
+from flask import url_for, send_from_directory
+import os
 
 app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def hello_world():
