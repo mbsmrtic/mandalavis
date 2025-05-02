1eba06e4162e6889f53f6e34a43707bd93f9168e
Mary Beth Smrtic <mbsmrtic@gmail.com>
2025-05-02 08:17:09 -0400

    .

--- app/static/js/mandalas/mandala16.js
+++ app/static/js/mandalas/mandala16.js
@@ -1,31 +1,35 @@
 import { Mandala } from "/static/js/mandala.js";
 import { Peapod } from "/static/js/shapes/peapod.js";
-import { initInteractions } from "/static/js/svg-interactions.js";
+// import { initInteractions } from "/static/js/svg-interactions.js";
 import { DotShape } from "/static/js/shapes/dot.js";
+import { SpiralShape } from "/static/js/shapes/spiral.js";
 
 const mandalaNum = '16';
 var mandalaId = "mandala" + mandalaNum;
 const mainElementId = mandalaId;      // `${mandalaId}-main`;
 
-initInteractions(mandalaNum);
+// initInteractions(mandalaNum);
 
-var mandala = new Mandala(mainElementId, 200, 200);
-mandala.addShape(new Peapod({
-    offset: 60,
-    width: 85,
-    length: 85,
-    howMany: 1,
-    toolTipText: 'peapod'
-}));
+var mandala = new Mandala(mainElementId, 100, 100);
+// mandala.addShape(new Peapod({
+//     offset: 60,
+//     width: 85,
+//     length: 85,
+//     howMany: 1,
+//     toolTipText: 'peapod'
+// }));
 
-mandala.addDot(20, 60);
-mandala.addDot(100, 10);
-mandala.addDot(180, 60);
-mandala.addDot(100, 110);
+// mandala.addDot(20, 60);
+// mandala.addDot(100, 10);
+// mandala.addDot(180, 60);
+// mandala.addDot(100, 110);
 
-mandala.addShape(new DotShape({
-    offset: 90,
-    width: 22,
-    length: 22,
-    howMany: 1,
-}));
+// mandala.addShape(new DotShape({
+//     offset: 90,
+//     width: 22,
+//     length: 22,
+//     howMany: 1,
+// }));
+
+var spiral = new SpiralShape({x: 100, y: 100, width: 100});
+mandala.addShape(spiral);
