591098d19ecd43c0a1294b3467f0e798b0c4eef7
Mary Beth Smrtic <mbsmrtic@gmail.com>
2025-05-05 19:10:15 -0400

    .

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import url_for, send_from_directory
 import os
-from app.src.mandaladata import getMandalaData
+# from app.src.mandaladata import getMandalaData
 
 app = Flask(__name__)
 
@@ -22,13 +22,8 @@ def header():
 @app.route('/post/<int:post_id>')
 def render_post(post_id):
     # mydata = getMandalaData(post_id)
-    mydata = {}
-    return render_template(f'post.html', post_id=post_id, mandalaData=mydata)
-
-@app.route('/post/py/<int:post_id>')
-def render_pypost(post_id):
-    mydata = getMandalaData(post_id)
-    return render_template(f'posts/pypost.html', post_id=post_id, mandalaData=mydata)
+    # mydata = {}
+    return render_template(f'post.html', post_id=post_id) #, mandalaData=mydata)
 
 @app.route('/testmandala')
 def render_testMandala():
