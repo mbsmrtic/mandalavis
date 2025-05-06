755f2c63aa5a457a8755d9009af65e4a0c568bc3
Mary Beth Smrtic <mbsmrtic@gmail.com>
2025-05-05 22:13:41 -0400

    including mandaladata python

---
+++ app/makesvg.py
@@ -0,0 +1,11 @@
+def makesvg():
+    svg_string = '<g id="mandala15" transform="matrix(1 0 0 1 0 0)">' + 
+        '<path mandalaid="mandala15" fill="#7AC74F" stroke="#388E3C" ' + 
+        'd="M20,60 Q100,10 180,60 Q100,110 20,60 Z">peapod</path>' + 
+        '<circle cx="20" cy="60" r="2" fill="black" transform="rotate(0 200 200)">' + 
+        '<title></title></circle><circle cx="100" cy="10" r="2" fill="black" ' + 
+        'transform="rotate(0 200 200)"><title></title></circle>' + 
+        '<circle cx="180" cy="60" r="2" fill="black" transform="rotate(0 200 200)">' + 
+        '<title></title></circle>' + 
+        '<circle cx="100" cy="110" r="2" fill="black" transform="rotate(0 200 200)">' + 
+    <title></title></circle><circle mandalaid="mandala15" cx="200" cy="88" r="17.6" fill="#666"></circle></g>
