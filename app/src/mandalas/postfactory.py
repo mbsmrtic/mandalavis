adac18626758bab055dd2d473272593de92f99bd
Mary Beth Smrtic <mbsmrtic@gmail.com>
2025-05-20 17:15:22 -0400

    .

--- app/src/mandalas/postfactory.py
+++ app/src/mandalas/postfactory.py
@@ -1,6 +1,6 @@
 from app.src.mandalas.mandala17 import MandalaPost17
 from app.src.mandalas.mandala16 import MandalaPost16
-from app.src.mandalas.mandala18 import MandalaPost18
+# from app.src.mandalas.mandala18 import MandalaPost18
 
 
 def mandala_post_factory(post_id): 
@@ -13,7 +13,7 @@ def mandala_post_factory(post_id):
     mandala_posts = {
         16: MandalaPost16,
         17: MandalaPost17,
-        18: MandalaPost18,
+        # 18: MandalaPost18,
     }
     try:
         return mandala_posts[post_id]()
